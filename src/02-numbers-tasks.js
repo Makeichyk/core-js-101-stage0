12fdf90a17a8c80aae29d11abe6c325815259a76
Jaroslav Makeichyk <jaroslau1@gmail.com>
2025-08-27 12:31:45 +0200

    feat: getDistanceBetweenPoints completed

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -68,8 +68,17 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  if (x1 === x2) {
+    return y1 - y2 < 0 ? (y1 - y2) * -1 : y1 - y2;
+  }
+  if (y1 === y2) {
+    return x1 - x2 < 0 ? (x1 - x2) * -1 : x1 - x2;
+  }
+  const x = x1 - x2 < 0 ? (x1 - x2) * -1 : x1 - x2;
+  const y = y1 - y2 < 0 ? (y1 - y2) * -1 : y1 - y2;
+
+  return Math.sqrt(x ** 2 + y ** 2);
 }
 
 /**
