47381378bdc932231fbbdcfff16f5790fd2b547e
Jaroslav Makeichyk <jaroslau1@gmail.com>
2025-08-30 16:22:55 +0200

    feat: getDigitalRoot completed

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -297,8 +297,13 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const sum = num
+    .toString()
+    .split('')
+    .reduce((acc, curr) => acc + +curr, 0);
+
+  return sum < 10 ? sum : getDigitalRoot(sum);
 }
 
 /**
